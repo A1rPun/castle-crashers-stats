17e8b618d1b0dc80afb0b4ca421f40e354a02ae0
A1rPun <thombakker@gmail.com>
2021-06-26 08:31:52 +0200

    :cyclone: "Improved" arena mode

--- src/App.vue
+++ src/App.vue
@@ -16,15 +16,15 @@
         <h3>Mode</h3>
         <label>
           <span>Normal</span>
-          <input type="radio" v-model="mode" :value="1" @change="changeMode" />
+          <input type="radio" v-model="mode" :value="1" @change="updateStats" />
         </label>
         <label>
           <span>Insane</span>
-          <input type="radio" v-model="mode" :value="2" @change="changeMode" />
+          <input type="radio" v-model="mode" :value="2" @change="updateStats" />
         </label>
         <label>
           <span>Arena</span>
-          <input type="radio" v-model="mode" :value="3" @change="changeMode" />
+          <input type="radio" v-model="mode" :value="3" @change="updateStats" />
         </label>
       </div>
       <div class="players" v-if="this.mode < 3">
@@ -46,7 +46,7 @@
           <input type="radio" v-model="numPlayers" :value="4" @change="updateStats" />
         </label>
       </div>
-      <div class="stats">
+      <div class="stats" v-if="this.mode < 3">
         <h3>Stats</h3>
         <label>
           <span>Level</span>
@@ -58,25 +58,24 @@
               min="1"
               max="99"
               @change="updateStats"
-              :disabled="this.mode === 3"
             />
           </div>
         </label>
         <label>
           <span>Strength</span>
-          <StatBar v-model="strength" @change="updateStats" :disabled="this.mode === 3"></StatBar>
+          <StatBar v-model="strength" @change="updateStats"></StatBar>
         </label>
         <label>
           <span>Magic</span>
-          <StatBar v-model="magic" @change="updateStats" :disabled="this.mode === 3"></StatBar>
+          <StatBar v-model="magic" @change="updateStats"></StatBar>
         </label>
         <label>
           <span>Defense</span>
-          <StatBar v-model="defense" @change="updateStats" :disabled="this.mode === 3"></StatBar>
+          <StatBar v-model="defense" @change="updateStats"></StatBar>
         </label>
         <label>
           <span>Agility</span>
-          <StatBar v-model="agility" @change="updateStats" :disabled="this.mode === 3"></StatBar>
+          <StatBar v-model="agility" @change="updateStats"></StatBar>
         </label>
       </div>
       <div>
@@ -141,7 +140,8 @@ rt m = magic</pre
         </div>
       </div>
       <h3>Stats total</h3>
-      <div>Exp: {{ experience }}</div>
+      <div v-if="this.mode < 3">Exp: {{ experience }}</div>
+      <div v-else>Level: 30</div>
       <div>Strength: {{ totalStrength }}</div>
       <div>Magic: {{ totalMagic }}</div>
       <div>Defense: {{ totalDefense }}</div>
@@ -186,7 +186,7 @@ export default {
   },
   computed: {
     extraStrength() {
-      return (this.weapon.strength ?? 0) + (this.pet.strength ?? 0) + this.level * 0.1;
+      return (this.weapon.strength ?? 0) + (this.pet.strength ?? 0) + (this.mode === 3 ? 30 : this.level) * 0.1;
     },
     extraMagic() {
       return (this.weapon.magic ?? 0) + (this.pet.magic ?? 0);
@@ -198,28 +198,28 @@ export default {
       return (this.weapon.agility ?? 0) + (this.pet.agility ?? 0);
     },
     totalStrength() {
-      return Math.max(Math.floor(this.strength + this.extraStrength), 1);
+      return Math.max(Math.floor((this.mode === 3 ? 20 : this.strength) + this.extraStrength), 1);
     },
     totalMagic() {
-      return Math.max(Math.floor(this.magic + this.extraMagic + this.level * 0.1), 1);
+      return Math.max(Math.floor((this.mode === 3 ? 20 : this.magic) + this.extraMagic + (this.mode === 3 ? 30 : this.level) * 0.1), 1);
     },
     totalDefense() {
-      return Math.max(Math.floor(this.defense + this.extraDefense), 1);
+      return Math.max(Math.floor((this.mode === 3 ? 30 : this.defense) + this.extraDefense), 1);
     },
     totalAgility() {
-      return Math.max(this.agility + this.extraAgility, 1);
+      return Math.max((this.mode === 3 ? 7.5 : this.agility) + this.extraAgility, 1);
     },
     normalAttack() {
-      return this.strength + basestats.normal;
+      return (this.mode === 3 ? 20 : this.strength) + basestats.normal;
     },
     heavyAttack() {
-      return this.strength * 1.15 + basestats.heavy;
+      return (this.mode === 3 ? 20 : this.strength) * 1.15 + basestats.heavy;
     },
     throwAttack() {
-      return this.strength * 1.2 + basestats.throw;
+      return (this.mode === 3 ? 20 : this.strength) * 1.2 + basestats.throw;
     },
     magicDamage() {
-      return Math.floor(Math.max(this.magic + this.extraMagic, 1) * 2 + this.level * 0.1);
+      return Math.floor(Math.max((this.mode === 3 ? 20 : this.magic) + this.extraMagic, 1) * 2 + (this.mode === 3 ? 30 : this.level) * 0.1);
     },
     splashDamage() {
       return Math.ceil(this.magicDamage * 0.5);
@@ -287,16 +287,6 @@ export default {
     },
     calcDamage: function (baseAttack) {
       return Math.floor(baseAttack + this.extraStrength) * (this.doCrit ? this.critMultiplier : 1);
-    },
-    changeMode() {
-      if (this.mode === 3) {
-        this.level = 30;
-        this.strength = 20;
-        this.magic = 20;
-        this.defense = 30;
-        this.agility = 7.5;
-      }
-      this.updateStats();
     },
     updateStats() {
       if (!this.weapon?.crit) this.doCrit = false;
